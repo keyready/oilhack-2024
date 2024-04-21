7a9427c9921d68eba0edf780be1d75c3841bbb9d
saneknapenek <kolesnikovag22@gmail.com>
2024-04-21 22:30:44 +0300

    added get result endpoin

--- server/sender/main.py
+++ server/sender/main.py
@@ -1,9 +1,11 @@
+import os
 import uuid
 
 from bottle import Bottle, request, HTTPResponse
 from errors import HTTPError
 from utils import send_task
 
+SAVE_DIR = '../media'
 
 app = Bottle()
 
@@ -28,11 +30,40 @@ def file_endpoint():
     send_task(str(id), file_bytes)
 
     response_ = HTTPResponse(
-        body=f'result_{id}.csv',
+        body=str(id),
         headers=cors_headers
     )
     return response_
 
+
+@app.get('/api/confirm')
+def get_result():
+    fileId = request.query.fileId
+
+    if "fileId" not in request.query_string or fileId == "":
+        raise HTTPError(
+            status=400,
+            msg="The request should contain a id parameter",
+            loc=["params", "fileId"]
+        )
+    
+    path_to_result = os.path.join(SAVE_DIR, f'result_{fileId}.csv')
+    if os.path.exists(path_to_result):
+        with open(path_to_result, 'rb') as file:
+            file_bytes = file.read()
+
+        cors_headers['Content-Type'] = 'text/csv'
+        response = HTTPResponse(
+            body=file_bytes,
+            headers=cors_headers
+        )
+        os.remove(path_to_result)
+    else:
+        response = HTTPResponse(
+            status=202,
+            headers=cors_headers
+        )
+    return response
 
 @app.error()
 def any_error(error):
